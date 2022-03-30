c9cba55815654bcf2d44fb533d5abc06561df101
Sajib Hossain <sajib.sj359@gmail.com>
2022-03-30 11:55:59 +0600

    adder route path

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import About from './Components/About/About';
+import Home from './Components/Home/Home';
+import NotFound from './Components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This Site made for Learning React Routing</h1>
+      <Routes>
+        <Route path='/' element={<About></About>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/*' element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
