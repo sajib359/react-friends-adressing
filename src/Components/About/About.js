459bd7d6ca9cf44ed5686f9446d7d7530b31d509
Sajib Hossain <sajib.sj359@gmail.com>
2022-03-30 11:40:20 +0600

    adeed react routing project

---
+++ src/Components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>Hi This is Sajib</h1>
+            <p>I wants to be a full stack Web Developer</p>
+        </div>
+    );
+};
+
+export default About;
